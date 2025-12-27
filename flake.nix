3c912be8773242d917a4861271054c3f99e810eb
ichi-h <85932406+ichi-h@users.noreply.github.com>
2025-12-27 15:26:14 +0900

    Add flake.nix for Koka.

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "koka";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        packages = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells = {
+          default = packages.mkShell {
+            buildInputs = with packages; [
+              koka
+            ];
+
+            shellHook = ''
+              koka --version
+            '';
+          };
+        };
+      }
+    );
+}
